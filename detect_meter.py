5101d0437a6ca3e7c1d56b47054aa254588ba04e
Roland Kahlert <roland.kahlert@pwc.com>
2020-05-26 20:35:22 +0200

    different API

--- detect_meter.py
+++ detect_meter.py
@@ -1,16 +1,13 @@
 import os
 import sys
-from pprint import pprint
-
-import requests
-# If you are using a Jupyter notebook, uncomment the following line.
-# %matplotlib inline
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
-from PIL import Image
-from io import BytesIO
 
 # Add your Computer Vision subscription key and endpoint to your environment variables.
+import time
+
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import TextOperationStatusCodes
+from msrest.authentication import CognitiveServicesCredentials
+
 if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
     subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
 else:
@@ -19,41 +16,33 @@ else:
     sys.exit()
 
 endpoint = 'https://detectmeter.cognitiveservices.azure.com/'
-ocr_url = endpoint + "vision/v2.1/ocr"
 
-image_path = "/Users/rkahlert001/Documents/Privat/Wasser/orig.png"
-# Read the image into a byte array
-image_data = open(image_path, "rb").read()
-# Set Content-Type to octet-stream
-headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
-params = {'language': 'unk', 'detectOrientation': 'true'}
-# put the byte array into your post request
-response = requests.post(ocr_url, headers=headers, params=params, data=image_data)
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
 
-response.raise_for_status()
 
-analysis = response.json()
-pprint(analysis)
+# Get an image with printed text
+image_stream = open('readme-images/water-capture.jpg', 'rb')
 
-# Extract the word bounding boxes and text.
-line_infos = [region["lines"] for region in analysis["regions"]]
-word_infos = []
-for line in line_infos:
-    for word_metadata in line:
-        for word_info in word_metadata["words"]:
-            word_infos.append(word_info)
+# Call API with URL and raw response (allows you to get the operation location)
+recognize_printed_results = computervision_client.batch_read_file_in_stream(image_stream, raw=True)
 
-# Display the image and overlay it with the extracted text.
-plt.figure(figsize=(5, 5))
-image = Image.open(image_path)
-ax = plt.imshow(image, alpha=0.5)
-for word in word_infos:
-    bbox = [int(num) for num in word["boundingBox"].split(",")]
-    text = word["text"]
-    origin = (bbox[0], bbox[1])
-    patch = Rectangle(origin, bbox[2], bbox[3],
-                      fill=False, linewidth=2, color='y')
-    ax.axes.add_patch(patch)
-    plt.text(origin[0], origin[1], text, fontsize=20, weight="bold", va="top")
-plt.show()
-plt.axis("off")
+
+# Get the operation location (URL with an ID at the end) from the response
+operation_location_remote = recognize_printed_results.headers["Operation-Location"]
+# Grab the ID from the URL
+operation_id = operation_location_remote.split("/")[-1]
+
+# Call the "GET" API and wait for it to retrieve the results
+while True:
+    get_printed_text_results = computervision_client.get_read_operation_result(operation_id)
+    if get_printed_text_results.status not in ['NotStarted', 'Running']:
+        break
+    time.sleep(1)
+
+# Print the detected text, line by line
+if get_printed_text_results.status == TextOperationStatusCodes.succeeded:
+    for text_result in get_printed_text_results.recognition_results:
+        for line in text_result.lines:
+            print(line.text)
+            print(line.bounding_box)
+print()
