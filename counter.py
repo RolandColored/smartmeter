0b6e2d7dd4de5e875d8b663ab32eeb2db5f68ca6
Roland Kahlert <roland.kahlert@pwc.com>
2021-11-18 21:10:21 +0100

    updated and simplified script

--- counter.py
+++ counter.py
@@ -5,19 +5,14 @@ try:
     import RPi.GPIO as GPIO
 except ImportError:
     from fake_rpi.RPi import GPIO
-try:
-    from picamera import PiCamera
-except ImportError:
-    from fake_rpi.picamera import PiCamera
 
 
-def setup_gas_sensor(file_handle):
+def setup_gas_sensor():
     gas_sensor_pin = 7
 
     def gas_callback(_):
         if GPIO.input(gas_sensor_pin) == 1:
-            file_handle.write(str(datetime.datetime.now())+',0.01\n')
-            file_handle.flush()
+            print('check')
 
     # Use physical pin numbering
     GPIO.setmode(GPIO.BOARD)
@@ -26,23 +21,5 @@ def setup_gas_sensor(file_handle):
     GPIO.add_event_detect(gas_sensor_pin, GPIO.RISING, callback=gas_callback)
 
 
-def capture_water_images():
-    camera = PiCamera(resolution=(2592, 1944))
-
-    # Camera warm-up time
-    time.sleep(2)
-
-    for filename in camera.capture_continuous('../data/water/{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
-        print('Captured %s' % filename)
-        time.sleep(600)
-
-
 if __name__ == '__main__':
-    with open('../data/gas.csv', 'a') as fh:
-        setup_gas_sensor(fh)
-
-        try:
-            capture_water_images()
-        finally:
-            print('Cleaning up')
-            GPIO.cleanup()
+    setup_gas_sensor()
